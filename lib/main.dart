d8f6138cb21da4c585e66765c4030dbf5cbf92f7
LeeGJ2 <wns4017@gmail.com>
2024-05-21 12:55:21 +0900

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
+import 'package:chat_app/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp(
+    appTheme: AppTheme(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({super.key, required this.appTheme});
+
+  final AppTheme appTheme;
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: appTheme.dark,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
